8c441890f0cd1318a088ad870a7c1a753cb86402
Awad Ahmed <awadahmed@Awads-MacBook-Air.local>
2020-10-02 18:48:22 -0400

    moving button to middle!

--- source code/lib/main.dart
+++ source code/lib/main.dart
@@ -16,30 +16,67 @@ void main()
 
 class CustomEmojiApp extends StatelessWidget
 {
-  
+
   @override
   Widget build(BuildContext context) {
-    
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
 
         appBar: AppBar(
-          backgroundColor: Colors.blue[200],
-
-          title: Text('Custom Emojiboard') //
-
+            backgroundColor: Colors.blue[200],
+            title: Text('Custom Emojiboard') //
         ),
 
 
         body: null,  //TODO: IMPLEMENT BODY WIDGETS
 
 
+
+        floatingActionButton: FloatingActionButton
+          (
+          onPressed: ()
+          {
+            //TODO: add onpressed code
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.deepPurple,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+
+
+
+        //bottom navbar
+        bottomNavigationBar: BottomNavigationBar(
+
+          backgroundColor: Colors.blue[200],
+
+          items:
+          [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('HOME'),
+
+            ),
+
+
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              title: Text('HOME'),
+
+            )
+
+          ],
+
+        ),
+
       ),
 
-
-
     );
+
   }
 }
 
