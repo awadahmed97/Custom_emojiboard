a58f5904a2f0b9c8a2e9d94c4b48f9f0b689a3d4
Raymond Chan <33289320+ray12125@users.noreply.github.com>
2020-11-24 19:56:54 -0500

    icon change
    new icon

--- Main Application/lib/MyBottomNavBar.dart
+++ Main Application/lib/MyBottomNavBar.dart
@@ -1,33 +1,38 @@
 import 'package:flutter/material.dart';
-
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class MyBottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 75,
-      padding: EdgeInsets.only(top: 15, bottom: 30),
+      padding: EdgeInsets.only(top: 10, bottom: 30),
       color: Colors.blue[400],
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget> [
+        children: <Widget>[
           IconButton(
-            icon: Icon(Icons.home, size: 44.0,),
+            icon: Icon(
+              Icons.home,
+              size: 44.0,
+            ),
             color: Colors.white,
             onPressed: () {
               Navigator.pushReplacementNamed(context, '/');
             },
           ),
           IconButton(
-            icon: Icon(Icons.camera, size: 44.0,),
+            icon: Icon(
+              Icons.mood,
+              size: 44.0,
+            ),
             color: Colors.white,
             onPressed: () {
               Navigator.pushReplacementNamed(context, '/other');
             },
           ),
-      ],
+        ],
       ),
-
     );
   }
 }
